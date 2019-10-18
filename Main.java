50c0d4caf8e628bbeb431060c1542ed45c180597
Nataly9898 <natalya.afimchenko@mail.ru>
2019-10-18 19:36:40 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,74 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.ApiContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class Main extends TelegramLongPollingBot {
+
+    public Main(DefaultBotOptions botOptions) {
+
+        super(botOptions);
+    }
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+
+        try {
+            DefaultBotOptions botOptions= ApiContext.getInstance(DefaultBotOptions.class);
+            botOptions.setProxyHost("188.187.62.173");
+            botOptions.setProxyPort(80);
+            botOptions.setProxyType(DefaultBotOptions.ProxyType.HTTP);
+            telegramBotsApi.registerBot(new Main(botOptions));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help")) {
+                sendMessage(message, "Hello");
+            } else
+                sendMessage(message, "Hello world");
+        }
+    }
+
+    private void sendMessage(Message message, String args) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText("Hello");
+        try {
+
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String getBotUsername() {
+
+        return "MessageNatBot";
+    }
+
+
+    public String getBotToken() {
+
+        return "912187838:AAE4zu7BAEKA1vrtUyb8dwEoKLA16hdI830";
+    }
+
+}
