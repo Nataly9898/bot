5aa69bf179c2f071690d61d72f16f7731e49c5ca
Nataly9898 <natalya.afimchenko@mail.ru>
2019-11-12 17:52:45 +0300

    bot

--- src/main/java/mainbot/Bot.java
+++ src/main/java/mainbot/Bot.java
@@ -1,44 +1,88 @@
+
 package mainbot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.ApiContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import org.telegram.abilitybots.api.bot.AbilityBot;
-import org.telegram.abilitybots.api.objects.Ability;
-import org.telegram.abilitybots.api.objects.Locality;
-import org.telegram.abilitybots.api.objects.Privacy;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Bot extends TelegramLongPollingBot{
+    public static final String TOKEN=System.getenv("ReminderNatalyBot");
 
+    public Bot(DefaultBotOptions botOptions) {
+        super(botOptions);
+    }
 
-public class Bot extends AbilityBot {
-    private static final String BOT_USERNAME = "ReminderNatalyBot";
-    private static final String BOT_TOKEN = "912187838:AAE4zu7BAEKA1vrtUyb8dwEoKLA16hdI830";
-    public Bot() {
-        super(BOT_TOKEN, BOT_USERNAME);
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+            botOptions.setProxyHost("129.146.181.251");
+            botOptions.setProxyPort(3128);
+            botOptions.setProxyType(DefaultBotOptions.ProxyType.HTTP);
+            telegramBotsApi.registerBot(new Bot(botOptions));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
     @Override
-    public int creatorId() {
-        return 9898;
+
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help")) {
+                sendMessage(message, "Hello");
+            } else
+                sendMessage(message, "Hello world");
+        }
     }
-    public Ability sayHelloWorld() {
-        return Ability
-                .builder()
-                .name("hello")
-                .info("says hello world!")
-                .input(0)
-                .locality(Locality.ALL)
-                .privacy(Privacy.PUBLIC)
-                .action(ctx-> silent.send("Hello World!", ctx.chatId()))
-                .post(ctx-> silent.send("Bye world!", ctx.chatId()))
-                .build();
-}
+    private void sendMessage(Message message, String args) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText("Hello");
+        try {
+            setButtons(sendMessage);
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void setButtons(SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+        keyboardFirstRow.add(new KeyboardButton("/setting"));
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+    }
 
+    @Override
+    public String getBotUsername() {
+        return "ReminderNatalyBot";
+    }
 
-   @Override
-   public String getBotUsername() {
-       return BOT_USERNAME;
-   }
     @Override
     public String getBotToken() {
-        return BOT_TOKEN;
+        return TOKEN;
     }
 
 }
